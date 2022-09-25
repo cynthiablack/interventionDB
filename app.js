b8011d77a71c8c546ec6c629163450ef230591dc
Cynthia Black <cindycblack@gmail.com>
2022-09-24 22:05:36 -0800

    Set EJS view engine

--- app.js
+++ app.js
@@ -10,9 +10,16 @@ connectDB()
 
 const app = express()
 
+// Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+// EJS view engine
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
