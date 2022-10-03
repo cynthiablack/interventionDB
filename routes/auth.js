82e56ac3d1b20ddbe54261e104c902820c3d38a1
Cynthia Black <cindycblack@gmail.com>
2022-10-02 16:00:45 -0800

    Fix logout function

--- routes/auth.js
+++ routes/auth.js
@@ -8,15 +8,21 @@ router.get('/google', passport.authenticate('google', { scope:['profile'] }))
 
 // @desc    Google auth callback
 // @route   GET /auth/google/callback
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/dashboard')
-})
+router.get(
+    '/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/' }), 
+    (req, res) => {
+        res.redirect('/dashboard')
+    }
+)
 
 // @desc    Logout user
 // @route   GET /auth/logout
-router.get('/logout', (req, res) => {
-    req.logout()
-    res.redirect('/')
+router.get('/logout', (req, res, next) => {
+    req.logout((error) => {
+        if (error) {return next(error)}
+        res.redirect('/')
+    })
 })
 
 module.exports = router
