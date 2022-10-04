f79b0fdbebfe28527612249d173fe4ac3309a5a0
Cynthia Black <cindycblack@gmail.com>
2022-10-03 17:30:55 -0800

    Show all students

--- routes/students.js
+++ routes/students.js
@@ -23,4 +23,43 @@ router.post('/', ensureAuth, async (req, res) => {
     }
   })
 
+  // @desc    Show all students
+// @route   GET /students
+router.get('/', ensureAuth, async (req, res) => {
+  try {
+    const students = await Student.find({ user: req.user.id }).lean()
+    res.render('students/index', {
+      name: req.user.displayName,
+      students,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
+  // @desc    Show one student
+// @route   GET /students/:id
+router.get('/:id', ensureAuth, (req, res) => {
+  res.send('one student')
+})
+
+// @desc    Show add intervention data page
+// @route   GET /students/data
+router.get('/data', ensureAuth, async (req, res) => {
+  res.send('data')
+  /*
+  try {
+    const students = await Student.find({ user: req.user.id }).lean()
+    res.render('/students/data', {
+      name: req.user.displayName,
+      students,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+  */
+})
+
 module.exports = router
